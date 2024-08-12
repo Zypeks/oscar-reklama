592f45f01b3a8fbbe99169aeb99f4a0abc66dd5f
adis0n <112665233+Zypeks@users.noreply.github.com>
2024-08-12 15:37:07 +0200

    Big Update for website

--- app.js
+++ app.js
@@ -1,14 +1,72 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav_links");
-    const navLinks = document.querySelectorAll(".nav_links li");
-    burger.addEventListener('click',()=>{
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
         nav.classList.toggle('nav-active');
+    
+        //Animate links
+        navLinks.forEach((link,index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
     });
-    navLinks.forEach(link,index=>{
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
-        console.log(index/7);
+}
+const navstick = () => {
+    window.addEventListener('scroll',function(){
+        var header = document.querySelector('header');
+        header.classList.toggle('sticky',window.scrollY > 0);
     })
+};
+navstick();
+navSlide();
+
+
+const slider = document.querySelector('.slider');
+const prevSlideBtn = document.querySelector('.prev-slide');
+const nextSlideBtn = document.querySelector('.next-slide');
+
+let slideIndex = 0;
+const slideCount = slider.querySelectorAll('.slide').length;
+
+function showSlide() {
+  slider.style.transform = `translateX(-${slideIndex * 100}%)`;
 }
 
-navSlide();
+showSlide();
+
+prevSlideBtn.addEventListener('click', () => {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slideCount - 1;
+  }
+  showSlide();
+});
+
+nextSlideBtn.addEventListener('click', () => {
+  slideIndex++;
+  if (slideIndex >= slideCount) {
+    slideIndex = 0;
+  }
+  showSlide();
+});
+
+
+var menuItems = document.querySelectorAll('.produkty');
+
+for (var i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('mouseover', function() {
+    this.classList.add('show');
+  });
+
+  menuItems[i].addEventListener('mouseout', function() {
+    this.classList.remove('show');
+  });
+}
